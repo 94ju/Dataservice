34215b8e8d13d8847d983434880fdc502a86956a
94ju <janithherath94@gmail.com>
2019-05-09 12:51:14 +0530

    urlchanged

--- node_apipractise/indx.js
+++ node_apipractise/indx.js
@@ -5,13 +5,13 @@ const app =express();
 const statetocodeUrl="https://gist.githubusercontent.com/PhantomGrin/a1e8ad30915ecd9d2659400d496d1ed6/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_titlecase.json";
 const codetostateUrl="https://gist.githubusercontent.com/PhantomGrin/a1e8ad30915ecd9d2659400d496d1ed6/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json";
 
-app.get('/statetocode',async(req,res)=>{
+app.get('/getstate',async(req,res)=>{
     const statetocodeData= await fetch(statetocodeUrl);
     const jsonobject = await statetocodeData.json();
     res.send(console.log(jsonobject));
 });
 
-app.get('/codetostate',async(req,res)=>{
+app.get('/getcode',async(req,res)=>{
     const codetostateData = await fetch(codetostateUrl);
     const jsonobject=await codetostateData.json();
     res.send(console.log(jsonobject));
