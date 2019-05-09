f6544dda260a1d0b3438c852f0b9f0e80ceae913
94ju <janithherath94@gmail.com>
2019-05-09 11:07:08 +0530

    statetocode endpoint

---
+++ node_apipractise/indx.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app =express();
+
+const statetocodeUrl="https://gist.githubusercontent.com/PhantomGrin/a1e8ad30915ecd9d2659400d496d1ed6/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_titlecase.json";
+const codetostateUrl="https://gist.githubusercontent.com/PhantomGrin/a1e8ad30915ecd9d2659400d496d1ed6/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json";
+
+app.get('/statetocode',async(req,res)=>{
+    const statetocodeData= await fetch(statetocodeUrl);
+    const jsonobject = await statetocodeData.json();
+    res.send(console.log(jsonobject));
+});
+
+app.get('/codetostate',(req,res)=>{
+    res.send(console.log("its working"));
+})
+
+app.listen(3000);
